e03e28d54963c9da707fdd6983b6c236e356c66b
slyen4425-cloud <slyen4425@gmail.com>
2026-09-18 15:04:36 +0200

    test: use literal inline script parser

--- tests/gens_phase2_inline_owner_manifest_v11411.test.cjs
+++ tests/gens_phase2_inline_owner_manifest_v11411.test.cjs
@@ -27,10 +27,7 @@ const disabled=new Set([
   'dungeonCore097StabilityRollbackDisabled'
 ]);
 
-const inlineScriptRe=new RegExp(
-  '<script\\\\b[^>]*\\\\bid=["\\\']([^"\\\']+)["\\\'][^>]*>([\\\\s\\\\S]*?)<\\\\/script>',
-  'gi'
-);
+const inlineScriptRe=/<script\b[^>]*\bid=["']([^"']+)["'][^>]*>([\s\S]*?)<\/script>/gi;
 const blocks=[...source.matchAll(inlineScriptRe)]
   .map((m,i)=>({id:m[1],source:m[2],order:i+1}));
 

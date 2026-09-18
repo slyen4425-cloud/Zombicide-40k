7e4e9a5edc0d7ecb6cd79f614ec5a8687938e2b1
slyen4425-cloud <slyen4425@gmail.com>
2026-09-18 11:41:00 +0200

    test: trace inline boot boundary in full Pages composition

--- tests/gens_phase2_full_composition_capture_browser_v11411.test.cjs
+++ tests/gens_phase2_full_composition_capture_browser_v11411.test.cjs
@@ -14,6 +14,10 @@ const pageTags=[...workflowBlock.matchAll(/<script src=\\?"([^"\\]+)[^>]*>/g)]
 assert.equal(pageTags.length,19,'Pages-equivalent characterization expects 19 injected modules');
 const perfTag='<script src="assets/gensrpg/gens-mobile-combat-performance-16781022.js"></script>';
 let productionHtml=indexSource.split(perfTag).join('');
+productionHtml=productionHtml.replace(
+  /(<script\\b[^>]*\\bid=["']([^"']+)["'][^>]*>)/gi,
+  (_m,open,id)=>open+`console.log("[phase2-inline-enter] ${String(id).replace(/["\\\\]/g,'_')}");`
+);
 productionHtml=productionHtml.replace('</body>',pageTags.join('\n')+'\n</body>');
 
 const CAPTURE_ID='gp_mt7ker7t_m2iw9';

80abf93d2ccaba827661c6293ad998b7af61b9d0
slyen4425-cloud <slyen4425@gmail.com>
2026-09-19 22:15:36 +0200

    test: align storage characterization with Core storage load

--- tests/gens_phase2_storage_ownership_characterization_v11411.test.cjs
+++ tests/gens_phase2_storage_ownership_characterization_v11411.test.cjs
@@ -35,7 +35,7 @@ while(queue.length){
   reachable.add(rel);
   for(const dep of assetRefs(read(rel)))if(!reachable.has(dep))queue.push(dep);
 }
-assert.equal(reachable.size,66);
+assert.equal(reachable.size,67);
 
 const disabled=new Set(Object.entries(inlineOwners.blocks||{}).filter(([,v])=>v.status==='disabled').map(([k])=>k));
 const inline=[...index.matchAll(/<script\b[^>]*\bid=["']([^"']+)["'][^>]*>([\s\S]*?)<\/script>/gi)]

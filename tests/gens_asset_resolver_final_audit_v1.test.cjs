edda07afe4acff4a40225dcaf40c54d5c686cabc
slyen4425-cloud <slyen4425@gmail.com>
2026-09-22 15:02:14 +0200

    test: realign asset resolver final audit after xpIntoLevel raccord

--- tests/gens_asset_resolver_final_audit_v1.test.cjs
+++ tests/gens_asset_resolver_final_audit_v1.test.cjs
@@ -11,7 +11,7 @@ const blob=crypto.createHash('sha1').update(Buffer.concat([
   bytes
 ])).digest('hex');
 
-assert.equal(blob,'a68bcbaf5d16bdbe2e70cbe0959a421371554fcc','asset resolver final audit must target the current post-Challenge-Library runtime index blob');
+assert.equal(blob,'8da7afa3c986f29e740eee1748dcc0ec0f8f75bc','asset resolver final audit must target the current post-Challenge-Library runtime index blob');
 
 function block(id){
   const m=index.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));

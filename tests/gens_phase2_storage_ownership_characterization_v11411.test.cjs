e137e8c25e85d4131897ec6ad5e5698887e6c07e
slyen4425-cloud <slyen4425@gmail.com>
2026-09-22 11:52:31 +0200

    test: advance storage graph count for Core Progression

--- tests/gens_phase2_storage_ownership_characterization_v11411.test.cjs
+++ tests/gens_phase2_storage_ownership_characterization_v11411.test.cjs
@@ -39,7 +39,7 @@ while(queue.length){
   reachable.add(rel);
   for(const dep of assetRefs(read(rel)))if(!reachable.has(dep))queue.push(dep);
 }
-assert.equal(reachable.size,77);
+assert.equal(reachable.size,78);
 
 const disabled=new Set(Object.entries(inlineOwners.blocks||{}).filter(([,v])=>v.status==='disabled').map(([k])=>k));
 const inline=[...index.matchAll(/<script\b[^>]*\bid=["']([^"']+)["'][^>]*>([\s\S]*?)<\/script>/gi)]

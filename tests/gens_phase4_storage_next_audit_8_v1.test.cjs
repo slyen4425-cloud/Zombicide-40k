08f4d06a4adea70423eab16d0e575ac093eb8855
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 23:30:06 +0200

    test: retarget runtime fingerprint after Core 0.28 retirement

--- tests/gens_phase4_storage_next_audit_8_v1.test.cjs
+++ tests/gens_phase4_storage_next_audit_8_v1.test.cjs
@@ -18,8 +18,8 @@ assert.deepEqual(manifest.totals,{
 const blob=crypto.createHash('sha1').update(Buffer.concat([
   Buffer.from('blob '+bytes.length+'\0'),bytes
 ])).digest('hex');
-assert.equal(bytes.length,8172500);
-assert.equal(blob,'7b586e9fb14b7a93a0edb069e115fd6d48cbda97');
+assert.equal(bytes.length,8170726);
+assert.equal(blob,'d9ee34d47fa888795db68cdc244d0c73d30ee523');
 
 function block(id){
   const m=src.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));
@@ -77,5 +77,5 @@ console.log(JSON.stringify({
   selected:'gensrpg_challenge_library_v1',
   selectedOwners:['dungeonCore051ExplorationPolish','dungeonCore200Rebuild','dungeonCore202ContentDensity'],
   selectedCoreAccesses:{reads:3,writes:2},
-  targetBlob:'7b586e9fb14b7a93a0edb069e115fd6d48cbda97'
+  targetBlob:'d9ee34d47fa888795db68cdc244d0c73d30ee523'
 },null,2));

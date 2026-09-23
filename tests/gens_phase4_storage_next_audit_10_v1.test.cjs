09cd8949501fa25ae251663e59db87f063685bbf
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 22:19:13 +0200

    test: retarget runtime fingerprint after Dungeon S2

--- tests/gens_phase4_storage_next_audit_10_v1.test.cjs
+++ tests/gens_phase4_storage_next_audit_10_v1.test.cjs
@@ -22,8 +22,8 @@ assert.deepEqual(manifest.byDomain.dungeon,{
 const blob=crypto.createHash('sha1').update(Buffer.concat([
   Buffer.from('blob '+bytes.length+'\0'),bytes
 ])).digest('hex');
-assert.equal(bytes.length,8172505);
-assert.equal(blob,'c17460335b2deb5e5916dbf91448b0706c38d0df');
+assert.equal(bytes.length,8172500);
+assert.equal(blob,'7b586e9fb14b7a93a0edb069e115fd6d48cbda97');
 
 function block(id){
   const m=src.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));

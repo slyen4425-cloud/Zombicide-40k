8616c38bd7ae02478b15af7b3829bb2cf1ee5c56
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 20:59:57 +0200

    test: retarget storage guard after Capture S1

--- tests/gens_phase4_storage_next_audit_10_v1.test.cjs
+++ tests/gens_phase4_storage_next_audit_10_v1.test.cjs
@@ -22,8 +22,8 @@ assert.deepEqual(manifest.byDomain.dungeon,{
 const blob=crypto.createHash('sha1').update(Buffer.concat([
   Buffer.from('blob '+bytes.length+'\0'),bytes
 ])).digest('hex');
-assert.equal(bytes.length,8170961);
-assert.equal(blob,'0c15b1dba66ce83f2b27ed99e371885fb1d0ed75');
+assert.equal(bytes.length,8172505);
+assert.equal(blob,'c17460335b2deb5e5916dbf91448b0706c38d0df');
 
 function block(id){
   const m=src.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));

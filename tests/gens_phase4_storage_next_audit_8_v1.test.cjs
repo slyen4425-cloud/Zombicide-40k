4b29cf3310b6e5ee9c65ca6cca869b2314f20403
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 20:59:46 +0200

    test: retarget storage guard after Capture S1

--- tests/gens_phase4_storage_next_audit_8_v1.test.cjs
+++ tests/gens_phase4_storage_next_audit_8_v1.test.cjs
@@ -18,8 +18,8 @@ assert.deepEqual(manifest.totals,{
 const blob=crypto.createHash('sha1').update(Buffer.concat([
   Buffer.from('blob '+bytes.length+'\0'),bytes
 ])).digest('hex');
-assert.equal(bytes.length,8170961);
-assert.equal(blob,'0c15b1dba66ce83f2b27ed99e371885fb1d0ed75');
+assert.equal(bytes.length,8172505);
+assert.equal(blob,'c17460335b2deb5e5916dbf91448b0706c38d0df');
 
 function block(id){
   const m=src.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));
@@ -77,5 +77,5 @@ console.log(JSON.stringify({
   selected:'gensrpg_challenge_library_v1',
   selectedOwners:['dungeonCore051ExplorationPolish','dungeonCore200Rebuild','dungeonCore202ContentDensity'],
   selectedCoreAccesses:{reads:3,writes:2},
-  targetBlob:'0c15b1dba66ce83f2b27ed99e371885fb1d0ed75'
+  targetBlob:'c17460335b2deb5e5916dbf91448b0706c38d0df'
 },null,2));

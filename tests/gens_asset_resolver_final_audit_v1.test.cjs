0af7d63433a62a5c88e73f981e5ef802fb0cf48f
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 10:37:24 +0200

    test: realign Asset Resolver final audit index blob

--- tests/gens_asset_resolver_final_audit_v1.test.cjs
+++ tests/gens_asset_resolver_final_audit_v1.test.cjs
@@ -11,7 +11,7 @@ const blob=crypto.createHash('sha1').update(Buffer.concat([
   bytes
 ])).digest('hex');
 
-assert.equal(blob,'f13835a2827dbfa9e2698cb026d3e732ad62aba4','asset resolver final audit must target the current post-Challenge-Library runtime index blob');
+assert.equal(blob,'0aaafb2eaf42b7bce6520efa633b6b6a6ffbe92a','asset resolver final audit must target the current post-Challenge-Library runtime index blob');
 
 function block(id){
   const m=index.match(new RegExp('<script[^>]*id=["\\\']'+id+'["\\\'][^>]*>([\\s\\S]*?)<\\/script>','i'));

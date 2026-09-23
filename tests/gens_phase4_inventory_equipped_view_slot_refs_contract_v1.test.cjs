27f8f660654bb3b1a7830d2b4d87d0490f3829bd
slyen4425-cloud <slyen4425@gmail.com>
2026-09-23 22:18:48 +0200

    test: retarget runtime fingerprint after Dungeon S2

--- tests/gens_phase4_inventory_equipped_view_slot_refs_contract_v1.test.cjs
+++ tests/gens_phase4_inventory_equipped_view_slot_refs_contract_v1.test.cjs
@@ -16,8 +16,8 @@ const bytes=Buffer.from(index,'utf8');
 const blob=crypto.createHash('sha1')
   .update(Buffer.concat([Buffer.from('blob '+bytes.length),Buffer.from([0]),bytes]))
   .digest('hex');
-assert.equal(bytes.length,8172505);
-assert.equal(blob,'c17460335b2deb5e5916dbf91448b0706c38d0df');
+assert.equal(bytes.length,8172500);
+assert.equal(blob,'7b586e9fb14b7a93a0edb069e115fd6d48cbda97');
 
 function extractFunction(source,name){
   const token='function '+name+'(';

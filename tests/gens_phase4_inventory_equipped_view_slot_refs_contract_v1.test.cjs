88eaee4981aa7e2a1a39c8589f5fd6c23f63de45
slyen4425-cloud <slyen4425@gmail.com>
2026-09-26 07:54:12 +0200

    test: align equipped-view fingerprint after builtin boundary

--- tests/gens_phase4_inventory_equipped_view_slot_refs_contract_v1.test.cjs
+++ tests/gens_phase4_inventory_equipped_view_slot_refs_contract_v1.test.cjs
@@ -16,8 +16,8 @@ const bytes=Buffer.from(index,'utf8');
 const blob=crypto.createHash('sha1')
   .update(Buffer.concat([Buffer.from('blob '+bytes.length),Buffer.from([0]),bytes]))
   .digest('hex');
-assert.equal(bytes.length,8170402);
-assert.equal(blob,'2a7dae75115d83b4edc368c42453a7cb58d0bd73');
+assert.equal(bytes.length,8170150);
+assert.equal(blob,'ca5cb0b92f4e6ff8779ebe2339bb2be32a89f8f9');
 
 function extractFunction(source,name){
   const token='function '+name+'(';
